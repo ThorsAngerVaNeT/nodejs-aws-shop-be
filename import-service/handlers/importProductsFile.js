1fc40f6ba3ec2ab38f16f953fd50e82a0cac89e8
Vadzim Antonau <vanet@tut.by>
2022-06-29 18:05:54 +0400

    feat: implement importProductsFile lambda

--- import-service/handlers/importProductsFile.js
+++ import-service/handlers/importProductsFile.js
@@ -1,10 +1,26 @@
 'use strict';
 import { constants as httpConstants } from 'http2';
+import AWS from 'aws-sdk';
 import { createResponse } from '../common/common';
+
+const { BUCKET_NAME, REGION } = process.env;
 
 export const handler = async event => {
   try {
-    return createResponse(httpConstants.HTTP_STATUS_OK, NULL);
+    const s3 = new AWS.S3({ region: REGION });
+    const { name: fileName } = event.queryStringParameters;
+    const catalogPath = `uploaded/${fileName}`;
+
+    const params = {
+      Bucket: BUCKET_NAME,
+      Key: catalogPath,
+      Expires: 60,
+      ContentType: 'text/csv',
+    };
+
+    const url = await s3.getSignedUrlPromise('putObject', params);
+
+    return createResponse(httpConstants.HTTP_STATUS_OK, url);
   } catch (error) {
     return createResponse(error.statusCode || httpConstants.HTTP_STATUS_INTERNAL_SERVER_ERROR, { error: error.message });
   }
