b12282309b2ff1bf9d1e02e976d358f76fd80fe3
Vadzim Antonau <vanet@tut.by>
2022-06-30 12:26:37 +0400

    feat: implement moving parsed csv to parsed folder

--- import-service/handlers/importFileParser.js
+++ import-service/handlers/importFileParser.js
@@ -1,15 +1,16 @@
 'use strict';
 import { constants as httpConstants } from 'http2';
-import { GetObjectCommand } from '@aws-sdk/client-s3';
+import { GetObjectCommand, CopyObjectCommand, DeleteObjectCommand } from '@aws-sdk/client-s3';
 import csv from 'csv-parser';
 import { BUCKET_NAME, createResponse } from '../common/common';
 import { s3Client } from '../common/s3Client';
 
 export const handler = async event => {
   try {
+    const { key: Key } = event.Records[0].s3.object;
     const params = {
       Bucket: BUCKET_NAME,
-      Key: event.Records[0].s3.object.key,
+      Key,
     };
 
     const command = new GetObjectCommand(params);
@@ -21,8 +22,9 @@ export const handler = async event => {
       .on('data', data => {
         console.log(data);
       })
-      .on('end', () => {
+      .on('end', async () => {
         console.log('CSV parsing is done!');
+        await moveFile(BUCKET_NAME, Key, 'parsed');
         return createResponse(httpConstants.HTTP_STATUS_OK, 'CSV parsing is done!');
       })
       .on('error', err => {
@@ -30,5 +32,31 @@ export const handler = async event => {
       });
   } catch (error) {
     return createResponse(error.statusCode || httpConstants.HTTP_STATUS_INTERNAL_SERVER_ERROR, { error: error.message });
+  }
+};
+
+const moveFile = async (bucketName, objectKey, destinationFolderName) => {
+  try {
+    const destinationKey = [destinationFolderName, ...objectKey.split('/').slice(1)].join('/');
+
+    const copyParams = {
+      Bucket: bucketName,
+      CopySource: `/${bucketName}/${objectKey}`,
+      Key: destinationKey,
+    };
+
+    const copyCommand = new CopyObjectCommand(copyParams);
+    await s3Client.send(copyCommand);
+
+    const deleteParams = {
+      Bucket: bucketName,
+      Key: objectKey,
+    };
+
+    const deleteCommand = new DeleteObjectCommand(deleteParams);
+    await s3Client.send(deleteCommand);
+  } catch (err) {
+    console.log('Error', err);
+    throw err;
   }
 };
