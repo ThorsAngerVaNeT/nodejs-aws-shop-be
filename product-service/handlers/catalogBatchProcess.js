ea1a003ff8d7fb405cc0ed7458c6a9b45fc8008d
Vadzim Antonau <vanet@tut.by>
2022-07-05 18:34:04 +0400

    refactor(product-service): catalogBatchProcess separate add products

--- product-service/handlers/catalogBatchProcess.js
+++ product-service/handlers/catalogBatchProcess.js
@@ -3,20 +3,32 @@ import { constants as httpConstants } from 'http2';
 import { createResponse } from '../common/common';
 import { create as createProduct } from '../db/product.pg.repository';
 
-export const handler = async event => {
+const addProducts = async products => {
   try {
-    const { Records: records } = event;
-
     const result = [];
 
-    for ({ body } of records) {
+    for (const { body } of products) {
       const productData = JSON.parse(body);
       const product = await createProduct(productData);
       if (product) result.push(product);
     }
 
+    return result;
+  } catch (error) {
+    console.log('error: ', error);
+    throw error;
+  }
+};
+
+export const handler = async event => {
+  try {
+    const { Records: records } = event;
+
+    const result = await addProducts(records);
+
     return createResponse(httpConstants.HTTP_STATUS_CREATED, result);
   } catch (error) {
+    console.log('error: ', error);
     return createResponse(error.statusCode || httpConstants.HTTP_STATUS_INTERNAL_SERVER_ERROR, { error: error.message });
   }
 };
